10ad2b4b2ab61f119fbe3f6e4aaaccb0e465724a
pauloancheta <paulo.ancheta@gmail.com>
2017-01-06 15:39:16 -0800

    Be able to delete an image from cloudinary

--- lib/cloudex/cloudinary_api/live.ex
+++ lib/cloudex/cloudinary_api/live.ex
@@ -3,6 +3,8 @@ defmodule Cloudex.CloudinaryApi.Live do
   The live API implementation for Cloudinary uploading
   """
 
+  @base_url "https://api.cloudinary.com/v1_1/"
+  @cloudinary_headers [{"Content-Type", "application/x-www-form-urlencoded"}, {"Accept", "application/json"}]
   @behaviour Cloudex.CloudinaryApi
 
   alias Cloudex.UploadedImage
@@ -40,6 +42,23 @@ defmodule Cloudex.CloudinaryApi.Live do
     {:error, "Upload/1 only accepts a String or {:ok, String}, received: #{inspect invalid_item}"}
   end
 
+  @doc """
+  Deletes an image given a public id
+  """
+  def delete(item) when is_bitstring(item) do
+    case delete_file(item) do
+      {:ok, _} -> {:ok, %Cloudex.DeletedImage{public_id: item}}
+      {:error, response} -> {:error, response.body}
+    end
+  end
+
+  @doc """
+  Catches error when public id was invalid
+  """
+  def delete(invalid_item) do
+    {:error, "delete/1 only accepts valid public id, received: #{inspect invalid_item}"}
+  end
+
   defp upload_file(file_path, opts) do
     body = {:multipart, (opts |> prepare_opts |> sign |> unify |> Map.to_list) ++ [{:file, file_path}]}
     body |> post(file_path)
@@ -52,6 +71,12 @@ defmodule Cloudex.CloudinaryApi.Live do
       |> sign
       |> URI.encode_query
       |> post(url)
+  end
+
+  defp delete_file(item) do
+    options = [hackney: [basic_auth: {Settings.get(:api_key), Settings.get(:secret)}]]
+    url = "#{@base_url}#{Settings.get(:cloud_name)}/resources/image/upload?public_ids[]=#{item}"
+    HTTPoison.delete(url, @cloudinary_headers, options)
   end
 
   defp post(body, source) do
